19a3a972064177dd37b9800fd79f9909b364767b
Shurim <ansondoeswork@gmail.com>
2021-07-01 23:35:22 -0700

    basic ping functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./auth.json');
+const command = require('./ping.js');
 
 client.on('ready', () => {
     console.log('the client is ready');
+
+    command(client, ['ping', 'test'], (message) => {
+        message.channel.send('Pong!');
+    });
 });
 
 client.login(config.token);
